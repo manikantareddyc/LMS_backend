9275d28d14e845e9867fecbaaca330d5dc9ab03c
manikantareddyc <manikanta.konamars@gmail.com>
2023-01-04 16:36:08 +0530

    s3 variable added

---
+++ s3-variable.tf
@@ -0,0 +1,27 @@
+variable "bucket_name" {}
+
+variable "acl_value" {
+
+    default = "public-read"
+
+}
+
+variable "aws_access_key" {
+
+default = “AKIAYP2AHH4N7SYKRPGF”
+
+}
+
+variable "aws_secret_key" {
+
+default = “SBingzvnHCG+KRfBu9I9gieY3klNT1l67VJViEBz”
+
+ }
+
+variable "region" {
+
+    default = "us-west-2"
+
+}
+
+
